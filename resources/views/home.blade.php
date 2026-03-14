3f44eb9a19ee0b77e3a9d2bb425d361faded3d0c
Stevano W.A <stevanowahyu016@gmail.com>
2026-03-14 13:45:47 +0700

    icon header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('images/ozj-logo.png') }}">
     <title>{{ __('home_title') }} - {{ config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
